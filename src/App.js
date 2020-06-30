9dfa71418c4e2e853c748ee69552cdcf4c615736
Iris Leal <iris_leal_m@hotmail.com>
2020-06-30 13:03:39 -0700

    Squeton working more

--- src/App.js
+++ src/App.js
@@ -6,23 +6,48 @@ function App() {
     <div>
       <Header />
       <Odometer />
-      <Footer />
-      
+      <Footer />      
     </div>
   );
 }
 
 
 class Odometer extends React.Component{
+    constructor(){
+      super();
+      this.state = {
+        odometer_value : 0,
+      }
+    }
+
+    updateOnes(operation){
+      let new_odometer_value = 100
+
+      this.setState({
+        odometer_value : new_odometer_value,
+      })
+    }
+
     render(){
-        return <h1>render</h1>
+        return <>
+            <h3>Please update the Odometer</h3>
+            
+            <p>Current value of the Odometer {this.state.odometer_value}</p>
+            <hr></hr>
+            <button onClick={() => this.updateOnes("substract")}> - </button> 
+            <label>Ones </label> 
+            <button onClick={() => this.updateOnes("substract")}> + </button> 
+            <hr></hr>
+         </>
     }
 }
 
 
 class Header extends React.Component{
   render(){
-      return <h1>Header</h1>
+      return <> 
+          <h1>Header</h1>
+          </>
   }
 }
 
